5dcd583acfeee8770dcd8d479d1366e688125060
dhanishgopi <110531324+dhanishgopi@users.noreply.github.com>
2023-10-09 11:52:23 +0530

    Delete foodofferings/mealmiracle directory

--- foodofferings/mealmiracle/src/main/java/com/repository/NpoDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.NpoDetails;
-
-public interface NpoDetailsRepository extends JpaRepository<NpoDetails, Long> {
-}
