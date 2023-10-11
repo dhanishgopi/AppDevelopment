c8b7aad499a7e352908b16593957e5a14a92adaf
dhanishgopi <110531324+dhanishgopi@users.noreply.github.com>
2023-10-11 14:11:25 +0530

    Add files via upload

---
+++ DAY 16/src/main/java/com/repository/NpoDetailsRepository.java
@@ -0,0 +1,10 @@
+package com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.entity.NpoDetails;
+
+
+public interface NpoDetailsRepository extends JpaRepository<NpoDetails, Long>{
+
+}
